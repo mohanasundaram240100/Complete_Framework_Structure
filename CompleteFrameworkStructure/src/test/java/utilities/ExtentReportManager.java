bd772caf2e338f2279dd14e7662a58f462a4f3f7
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-07 20:31:18 +0530

    DC

--- CompleteFrameworkStructure/src/test/java/utilities/ExtentReportManager.java
+++ CompleteFrameworkStructure/src/test/java/utilities/ExtentReportManager.java
@@ -1,6 +1,8 @@
 package utilities;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -12,6 +14,47 @@ public class ExtentReportManager implements ITestListener {
 	public ExtentReports extentreport;
 	public ExtentTest extenttest;
 	
+	
+	public void onStart(ITestContext context)
+	{
+		sparkreporter = new ExtentSparkReporter("ExtentReport.html");
+		sparkreporter.config().setDocumentTitle("Automation Report");
+		sparkreporter.config().setReportName("Functional Testing");
+		
+		extentreport = new ExtentReports();
+		extentreport.attachReporter(sparkreporter);
+		extentreport.setSystemInfo("Tester", "Selenium WebDriver");
+		
+				
+	}
+	
+	public void onTestSuccess(ITestResult result)
+	{
+		extenttest = extentreport.createTest(result.getName());
+		extenttest.pass("Test is Passed");
+		
+	}
+	
+	public void onTestFailure(ITestResult result)
+	{
+		extenttest = extentreport.createTest(result.getName());
+		extenttest.fail("Test is Failed");
+		
+	}
+	
+	public void onTestSkipped(ITestResult result)
+	{
+		extenttest = extentreport.createTest(result.getName());
+		extenttest.skip("Test is Skipped");
+		
+	}
+	
+	
+	public void onFinish(ITestContext context)
+	{
+		//extenttest.flush();
+		
+	}
 	
 	
 	
